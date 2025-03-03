5b7085c69bbec2a4506b7bec4a77fe31a8551aa8
Hongbin Li <hblee12294@gmail.com>
2025-03-02 18:18:02 -0800

    feat: improve styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Editor from "@/components/Editor";
 export default function Home() {
   return (
     <div>
-      <div className="max-w-screen-lg mx-auto">
+      <div className="max-w-screen-lg mx-auto px-4 pb-12">
         <Editor />
       </div>
     </div>
--- components/controls/BaseControl.tsx
+++ components/controls/BaseControl.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "@tiptap/react";
+import clsx from "clsx";
 
 interface BaseControlProps {
   editor: Editor;
@@ -18,9 +19,11 @@ export const BaseControl = ({
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`px-2 py-1 mr-1 rounded cursor-pointer ${
-        isActive ? "bg-gray-200" : "hover:bg-gray-100"
-      } ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
+      className={clsx(
+        "px-2 py-1 mr-1 rounded cursor-pointer",
+        isActive ? "bg-gray-200" : "hover:bg-gray-100",
+        disabled && "opacity-50 cursor-not-allowed"
+      )}
     >
       {children}
     </button>
